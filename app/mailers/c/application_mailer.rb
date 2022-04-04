fc641120f66ca4c237198c9dbaf76258c69645d3
Shane Morgan <shane@cd2solutions.co.uk>
2022-04-04 10:17:25 +0100

    Update application_mailer.rb

--- app/mailers/c/application_mailer.rb
+++ app/mailers/c/application_mailer.rb
@@ -2,7 +2,7 @@
 
 module C
   class ApplicationMailer < ActionMailer::Base
-    default from: "#{C.store_name}<notifications@#{C.domain_name}>"
+    default from: C.email_from ? C.email_from : "#{C.store_name}<notifications@#{C.domain_name}>"
     layout 'c/mailer'
   end
 end
